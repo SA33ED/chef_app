746fa385bf65ae559e1a1d649bb860a050c4b9f3
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-09-05 18:17:54 +0300

    app assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,23 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar
+  /// assets/images/avatar.svg
+  static const String imagesAvatar = "assets/images/avatar.svg";
+
+  /// Assets for imagesBg1
+  /// assets/images/bg1.png
+  static const String imagesBg1 = "assets/images/bg1.png";
+
+  /// Assets for imagesBg2
+  /// assets/images/bg2.png
+  static const String imagesBg2 = "assets/images/bg2.png";
+
+  /// Assets for imagesLock
+  /// assets/images/lock.svg
+  static const String imagesLock = "assets/images/lock.svg";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.svg
+  static const String imagesLogo = "assets/images/logo.svg";
+}
